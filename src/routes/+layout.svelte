7488e29b9b30563f942ecbbe9960b63f0bbd3d23
keeperofkey <mail@liamdodd.com>
2024-07-01 12:02:03 -0400

    feat(analytics): inject we anylitics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
     import "../app.css";
     import { onMount } from "svelte";
-    import { page } from "$app/stores";
+    // import { page } from "$app/stores";
     import Return from "$lib/return.svelte";
+    import { dev } from "$app/environment";
+    import { inject } from "@vercel/analytics";
+
+    inject({ mode: dev ? "development" : "production" });
 
     let isOpen: Boolean;
 
