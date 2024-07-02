9c2e3a558985ca7bb74f63881b26f5b8efc93ced
keeperofkey <doddtastic@gmail.com>
2024-07-02 09:12:08 -0400

    about-and-nav

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -5,33 +5,54 @@
     <title>Liam Dodd - About</title>
     <meta name="description" content="About me!" />
 </svelte:head>
-<main class="texts">
-    <h1>About</h1>
-    <p>
-        Hi there! Welcome to my personal website and portfolio. Everything you
-        see here is my own work including the code behind it. I hope you enjoy
-        it!
-    </p>
-    <p>It's a work in progress, so please check back often.</p>
-    <p>
-        I hope to showcase a variety of skills I have acquired throughout the
-        years. If you have any questions or comments, feel free to reach out to
-        me at <a href="mailto:mail@liamdodd.com">mail@liamdodd.com</a>.
-    </p>
-    <h2>Tools Used</h2>
-    <ul>
-        <li><a href="https://svelte.dev/">Svelte</a></li>
-        <li><a href="https://kit.svelte.dev/">Svelte-Kit</a></li>
-        <li><a href="https://threejs.org/">Three.js</a></li>
-        <li><a href="https://www.sidefx.com/">Houdini</a></li>
-        <li>
-            <a href="https://github.com/huggingface/gsplat.js/">Gsplat.js</a>
-        </li>
-        <li><a href="https://nerf.studio/">Nerfstudio</a></li>
-        <li><a href="https://vercel.com/">Vercel</a></li>
-        <li><a href="https://neovim.io/">Neovim</a></li>
-    </ul>
-</main>
+<div
+    class="grid grid-cols-1 grid-rows-3 h-dvh items-center justify-items-center"
+>
+    <main
+        class="p-4 row-start-2 max-w-prose bg-slate-100 bg-opacity-75 rounded-2xl"
+    >
+        <h1>About</h1>
+        <p>
+            Hi there! Welcome to my personal website and portfolio. Everything
+            you see here is my own work including the code behind it. I hope you
+            enjoy it!
+        </p>
+        <p>It's a work in progress, so please check back often.</p>
+        <p>
+            I hope to showcase a variety of skills I have acquired throughout
+            the years. If you have any questions or comments, feel free to reach
+            out to me at <a href="mailto:mail@liamdodd.com">mail@liamdodd.com</a
+            >.
+        </p>
+        <h2>Tools Used</h2>
+        <ul>
+            <li><a class="underline" href="https://svelte.dev/">Svelte</a></li>
+            <li>
+                <a class="underline" href="https://kit.svelte.dev/"
+                    >Svelte-Kit</a
+                >
+            </li>
+            <li>
+                <a class="underline" href="https://threejs.org/">Three.js</a>
+            </li>
+            <li>
+                <a class="underline" href="https://www.sidefx.com/">Houdini</a>
+            </li>
+            <li>
+                <a
+                    class="underline"
+                    href="https://github.com/huggingface/gsplat.js/"
+                    >Gsplat.js</a
+                >
+            </li>
+            <li>
+                <a class="underline" href="https://nerf.studio/">Nerfstudio</a>
+            </li>
+            <!-- <li><a class="underline" href="https://vercel.com/">Vercel</a></li> -->
+            <li><a class="underline" href="https://neovim.io/">Neovim</a></li>
+        </ul>
+    </main>
+</div>
 
 <style>
 </style>
