4abea2afbcb56ead401f00f09ff0dda5686a6488
keeperofkey <mail@liamdodd.com>
2024-07-14 13:38:51 -0400

    clean-tests

--- src/routes/test/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-    import ScrollOrbit from "$lib/scroll-orbit.svelte";
-    import Gall from "$lib/gall.svelte";
-    import images from "$lib/images";
-    let data = images.senior.data;
-</script>
-
-<Gall images={data} />
-<ScrollOrbit modelName="senior.glb" splatName="senior.splat" />
